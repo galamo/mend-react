35d61fae4ccbc925b908dfcda8ddae382cf9e632
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-04-04 14:58:36 +0300

    answer

--- examples/typescript/src/generics_ex.ts
+++ examples/typescript/src/generics_ex.ts
@@ -29,36 +29,26 @@ const users: User[] = [
   { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
 ];
 
-export type ApiResponse<T> = unknown;
-
-type AdminsApiResponse =
+export type ApiResponse<T> =
   | {
       status: "success";
-      data: Admin[];
+      data: T[];
     }
   | {
       status: "error";
       error: string;
     };
 
-export function requestAdmins(callback: (response: AdminsApiResponse) => void) {
+export function requestAdmins(
+  callback: (response: ApiResponse<Admin>) => void
+) {
   callback({
     status: "success",
     data: admins,
   });
 }
 
-type UsersApiResponse =
-  | {
-      status: "success";
-      data: User[];
-    }
-  | {
-      status: "error";
-      error: string;
-    };
-
-export function requestUsers(callback: (response: UsersApiResponse) => void) {
+export function requestUsers(callback: (response: ApiResponse<User>) => void) {
   callback({
     status: "success",
     data: users,
